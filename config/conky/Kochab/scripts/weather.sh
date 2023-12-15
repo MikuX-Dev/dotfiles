c480f6bbfc4bec37b9b5327cc13791d9a12fb548
MikuX-Dev <abinishanth3464@gmail.com>
2023-12-15 17:27:19 +0530

    .

--- config/conky/Kochab/scripts/weather.sh
+++ config/conky/Kochab/scripts/weather.sh
@@ -5,9 +5,9 @@
 # so that conky will still display the weather when offline even though it doesn't up to date
 
 # you can use this or replace with yours
-api_key=API_KEY
+api_key=912416a3726142366d9456d54cfd594b
 # get your city id at https://openweathermap.org/find and replace
-city_id=CITY_ID
+city_id=1273865
 
 url="api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${api_key}&cnt=5&units=metric&lang=en"
 curl ${url} -s -o ~/.cache/weather.json
